60e579a03fdb82cea0bac5519389fcd45cfd934c
Faip Mardoni <faipmardoni@gmail.com>
2018-03-07 07:17:00 +0700

    Pasangan Terbesar

--- exercise-8.js
+++ exercise-8.js
@@ -13,11 +13,11 @@ function pasanganTerbesar(num) {
     numPair.pop()
     numPair.sort(function(value1,value2){return value1<value2});
     return Number(numPair[0]);
-  }
-  
-  // TEST CASES
-  console.log(pasanganTerbesar(641573)); // 73
-  console.log(pasanganTerbesar(12783456)); // 83
-  console.log(pasanganTerbesar(910233)); // 91
-  console.log(pasanganTerbesar(71856421)); // 85
-  console.log(pasanganTerbesar(79918293)); // 99
+}
+
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
