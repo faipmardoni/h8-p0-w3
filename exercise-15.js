e198a9be66f5232a90ba1d509eeafbc112e51659
Faip Mardoni <faipmardoni@gmail.com>
2018-03-08 20:54:37 +0700

    menghapus if pertama

--- exercise-15.js
+++ exercise-15.js
@@ -3,17 +3,11 @@ function groupAnimals(animals) {
     var arrAnimal = [];
     for (var i=0; i<animals.length; i++) {
         var arrK = [];
-        if(animals[i+1]===undefined) {
-            if(animals[i][0]!==animals[i-1][0]) {
-                arrK.push(animals[i]);
-                arrAnimal.push(arrK);
-            }            
-        }else if(animals[i-1]==undefined) {
+        if(animals[i-1]==undefined) {
             arrK.push(animals[i])  
             for (var j=i+1; j<animals.length; j++) {
                 if(animals[i][0]===animals[j][0]) {
-                    arrK.push(animals[j]);
-                                     
+                    arrK.push(animals[j]);                     
                 }    
             }
             arrAnimal.push(arrK);
@@ -31,7 +25,7 @@ function groupAnimals(animals) {
   }
   
   // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'kucing']));
   // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
   // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
