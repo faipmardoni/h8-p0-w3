1dbd4f6e0f827315df6bb3f3122bb1f647e9fcdc
Faip Mardoni <faipmardoni@gmail.com>
2018-03-12 08:39:33 +0700

    revisi

--- exercise-11.js
+++ exercise-11.js
@@ -3,6 +3,7 @@ function tentukanDeretAritmatika(arr) {
     for (var i=arr.length-2; i>0; i--) {
         deret.push(arr[arr.length-1]-arr[arr.length-2]===arr[i]-arr[i-1])
         deret.sort()
+        console.log(deret)
     }
     return deret[0]==true
   }
@@ -13,4 +14,3 @@ function tentukanDeretAritmatika(arr) {
   console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
   console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
   console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
-  console.log(tentukanDeretAritmatika([3, 7, 8, 12, 5, 9]));
--- exercise-15.js
+++ exercise-15.js
@@ -1,8 +1,29 @@
 function groupAnimals(animals) {
     animals.sort();
     var arrAnimal = [];
+
+    // Versi Efisien
+    // var awal = animals[0][0];
+    // var arrK = []
+    // for (var i=0; i<animals.length; i++) {
+    //     if(awal==animals[i][0]) {
+    //         arrK.push(animals[i]);
+    //     }else {
+    //         arrAnimal.push(arrK);
+    //         arrK = [];
+    //         arrK.push(animals[i]);
+    //         awal = animals[i][0]; 
+    //     }
+    // }
+    // arrAnimal.push(arrK);
+    // return arrAnimal;
+
+    // --------------------------
+
+    //Versi FAIP MARDONI
     for (var i=0; i<animals.length; i++) {
         var arrK = [];
+        console.log(animals)
         if(animals[i-1]==undefined) {
             arrK.push(animals[i])  
             for (var j=i+1; j<animals.length; j++) {
@@ -11,6 +32,7 @@ function groupAnimals(animals) {
                 }    
             }
             arrAnimal.push(arrK);
+            
         }else if (animals[i][0]!==animals[i-1][0]) {
             arrK.push(animals[i])  
             for (var j=i+1; j<animals.length; j++) {
